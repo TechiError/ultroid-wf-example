770c9432fd7900dfbff6d6be2018365416cd2226
Pratham <97892227+SoaringSeeker14@users.noreply.github.com>
2022-03-14 19:09:31 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,5 @@
-branch=main
-git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp ultroid/.env /root/TeamUltroid/.env
+branch=dev
+git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroidcp ultroid/.env /root/TeamUltroid/.envcd /root/TeamUltroiddocker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroiddocker run --privileged --env-file .env --rm -i ultroid
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
