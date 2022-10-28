8fa3ac1f59ef01737cbb263f8576807918e7ea4b
TechiError <techierror@gmail.com>
2022-10-28 12:19:22 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -5,4 +5,12 @@ if grep -q MONGO_URI /root/TeamUltroid/.env; then
 elif grep -q REDIS_URI /root/TeamUltroid/.env; then
     pip3 install redis hiredis
 fi
-bash startup
+trap 'exit()'
+timeout --preserve-status 19500 bash startup &
+pid=$!
+wait $pid
+if [ $? -eq 143 ]; then
+    exit 0
+else
+    exit 1
+fi
