ab6dab041651a2cde7095972a1bdc1b02d3542a5
TechiError <techierror@gmail.com>
2022-12-31 15:26:11 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,10 +1,5 @@
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid && ls
-if grep -q MONGO_URI /root/TeamUltroid/.env; then
-    pip3 install pymongo
-elif grep -q REDIS_URI /root/TeamUltroid/.env; then
-    pip3 install redis hiredis
-fi
 trap 'exit()'
 timeout --preserve-status 19500 bash startup &
 pid=$!
